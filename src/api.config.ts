ece003f279dd17c9b0eff47aec6423c91298a3ff
parvej-shah <parvejshahlabib007@gmail.com>
2026-06-17 22:42:25 +0600

    api

--- src/api.config.ts
+++ src/api.config.ts
@@ -1,3 +1,3 @@
-//export const BACKEND_URL = "https://mathpro-backend-drnm.onrender.com";
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL = "https://mathpro-backend-drnm.onrender.com";
+//export const BACKEND_URL = "http://localhost:8000";
 export const COURSE_ID = "1";
